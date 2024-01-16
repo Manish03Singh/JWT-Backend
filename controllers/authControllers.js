0029a3214d4573ba5f5eefac007634f1613cc1f5
Manish03Singh <manish031627@gmail.com>
2024-01-16 20:34:44 +0530

    first commit

---
+++ controllers/authControllers.js
@@ -0,0 +1,74 @@
+const User = require("../models/authModel");
+const jwt = require("jsonwebtoken");
+const dotenv = require('dotenv').config()
+
+const maxAge = 30;
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: maxAge,
+  });
+};
+
+const handleErrors = (err) => {
+  let errors = { email: "", password: "" };
+
+  console.log(err);
+  if (err.message === "incorrect email") {
+    errors.email = "Email is not registered";
+  }
+
+  if (err.message === "incorrect password") {
+    errors.password = "Password is incorrect";
+  }
+
+  if (err.code === 11000) {
+    errors.email = "Email is already registered";
+    return errors;
+  }
+
+  if (err.message.includes("Users credentials wrong")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+
+    res.cookie("jwt", token, {
+      withCredentials: true,
+      httpOnly: false,
+      maxAge: maxAge * 1000,
+    });
+
+    res.status(201).json({ user: user._id, created: true });
+  } catch (err) {
+    console.log(err);
+    const errors = handleErrors(err);
+    res.json({ errors, created: false });
+  }
+};
+
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: false, maxAge: maxAge * 1000 });
+    const _date = Date.now()
+    const todo = await User.findByIdAndUpdate(
+      {_id:user._id},
+      {time:_date}
+    )
+    res.status(200).json({ user: user._id, status: true });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.json({ errors, status: false });
+  }
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const dotenv = require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server Started Successfully on port ${port}.`);
+  }
+});
+
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(express.json());
+app.use("/", authRoutes);
